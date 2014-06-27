2bdf269338bf001050755663a74cde7054fc2079
Piero Wbmstr <me@e-piwi.fr>
2014-06-27 16:32:36 +0200

    ssh synchro user->root script

---
+++ sync-ssh-root.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# synch-ssh-root.sh
+# by @pierowbmstr (me at e-piwi dot fr)
+# <http://github.com/piwi/binaires.git>
+# (personal) file licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>
+#
+# Synchronize an SSH config between a user and root on a machine
+#
+
+_usrname="${1:-piwi}"
+ROOT_SSHDIR=/root/.ssh/
+USER_SSHDIR=/home/${_usrname}/.ssh/
+
+# sudo 
+if [ "`id -u`" != "0" ]; then
+    echo "!! > You must run this script as root !";
+    exit 1
+fi
+
+# let's go
+
+echo "find ${USER_SSHDIR} -exec cp -f {} ${ROOT_SSHDIR} \;"
+sudo find ${USER_SSHDIR} -exec cp -f {} ${ROOT_SSHDIR} \;
+
+echo "find ${ROOT_SSHDIR} -regex '.*_\(rsa\|dsa\)' -exec chmod 600 {} \;"
+sudo find ${ROOT_SSHDIR} -regex ".*_\(rsa\|dsa\)" -exec chmod 600 {} \;
+
+exit 0
+
+# Endfile
