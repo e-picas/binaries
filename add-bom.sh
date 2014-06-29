2ba84f8604a0f43e9329e465e36cd39427fad1e0
piwi <me@e-piwi.fr>
2014-06-29 20:25:36 +0200

    review of the AddBom script

--- add-bom.sh
+++ add-bom.sh
@@ -5,25 +5,57 @@
 # <http://github.com/piwi/binaires.git>
 # (personal) file licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>
 #
-# Add the BOM marker at the top of a file
+# Add the BOM marker at the top of a file or dir contents
 #
+
+usage () {
+    echo "usage: $0 <dir/file> [<mask=*.php>] [<target_filename=%s>]"
+}
+
+processOneFile () {
+    local FILESOURCE="${1}"
+    if [ "${FILESOURCE}" == "${FILETARGET}" ]||[ '%s' == "${FILETARGET}" ]
+    then
+        TMPFILE="${FILETARGET}.tmp"
+        printf '\xEF\xBB\xBF' > "${TMPFILE}"
+        cat "${FILESOURCE}" >> "${TMPFILE}"
+        rm -f "${FILESOURCE}" && mv "${TMPFILE}" "${FILETARGET}"
+        echo "${FILETARGET} `file -bi "${FILETARGET}"`"
+    else
+        printf '\xEF\xBB\xBF' > "${FILETARGET}"
+        cat "${FILESOURCE}" >> "${FILETARGET}"
+        echo "${FILETARGET} `file -bi "${FILETARGET}"`"
+    fi
+}
 
 if [ $# -eq 0 ]; then
     echo "> missing arg!"
-    echo "usage: $0 <working_dir/file> [<mask=*.php>]"
+    usage
     exit 1
 fi
 
-_DIR="$1"
-_MASK="${2:-*.php}"
+if [ "${1}" == '-h' ]||[ "${1}" == '--help' ]
+then
+    usage
+    exit 0
+fi
 
-for _f in `find "${_DIR}" -type f -name "${_MASK}"`; do
-    echo ${_f}
-    _f_tmp=${_f}.tmp
-    printf '\xEF\xBB\xBF' > ${_f_tmp}
-    cat ${_f} >> ${_f_tmp} 
-    rm -f ${_f} && mv ${_f_tmp} ${_f}
-done
+FILESOURCE="${1}"
+FILEMASK="${2:-*.php}"
+FILETARGET="${3:-%s}"
+
+if [ -f "$FILESOURCE" ]; then
+    processOneFile "${FILESOURCE}"
+
+elif [ -d "$FILESOURCE" ]
+    for _f in `find "${FILESOURCE}" -type f -name "${FILEMASK}"`; do
+        processOneFile "${_f}"
+    done
+
+else
+    echo "path '${FILESOURCE}' not found!"
+    exit 1
+fi
 
 exit 0
 # Endfile
