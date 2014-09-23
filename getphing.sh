a6660260fa02e59662f3e1db7c65173bed760855
piwi <me@e-piwi.fr>
2014-09-23 09:38:31 +0200

    new script to get the 'phing.phar' binary

---
+++ getphing.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# getphing.sh
+# by @pierowbmstr (me at e-piwi dot fr)
+# <http://github.com/piwi/binaires.git>
+# (personal) file licensed under CC BY-NC-SA 4.0 <http://creativecommons.org/licenses/by-nc-sa/4.0/>
+#
+# Get the `phing.phar` package
+#
+
+if [ "$1" = '-h' ] || [ "$1" = '--help' ]; then
+    echo
+    echo "## PHING installer ##"
+    echo "This script will get and install last phing.phar package version."
+    echo
+    echo "# Usage:"
+    echo "        ~$ sh $0 [options]"
+    echo
+    echo "# Options:"
+    echo "        --install-dir     installation directory (default is current)"
+    echo "        --filename        installation filename (default is 'phing.phar')"
+    echo
+    echo "##"
+    echo
+    exit 0
+fi
+
+_TARGETDIR="${1:-.}"
+_TARGETFILENAME="${2:-phing.phar}"
+
+curl -L http://www.phing.info/get/phing-latest.phar > "${_TARGETDIR}/${_TARGETFILENAME}"
+
+# End
+
